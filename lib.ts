75f54d9fb828844e2f45b4643e331bd7c716746f
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-02-07 12:18:26 +0100

    z7beta update

--- lib.ts
+++ lib.ts
@@ -81,8 +81,9 @@ function removeElements() {
   }
 }
 
-if (Zotero.platformMajorVersion < 102 && typeof URL === 'undefined') {
-  Cu.importGlobalProperties(['URL'])
+if (Zotero.platformMajorVersion < 102) {
+  const props = ['URL', 'Blob', 'FormData'].filter(p => typeof p === 'undefined')
+  if (props.length) Cu.importGlobalProperties(props)
 }
 
 log('AltOpen PDF: lib loading')
@@ -147,90 +148,98 @@ Zotero.AltOpenPDF = Zotero.AltOpenPDF || new class ZoteroAltOpenPDF {
       try {
         let sibling: HTMLElement
         for (const mi of menu.children as unknown as HTMLElement[]) {
-          if (mi.style?.listStyleImage !== 'url("chrome://zotero/skin/treeitem-attachment-pdf.png")') continue
-
-          if (mi.getAttribute('zotero-open-pdf-external')) {
-            sibling = null
-            break
+          if (mi.getAttribute('data-open-pdf-alternate')) { // already in menu
+            return
           }
 
-          sibling = mi
+          if (mi.getAttribute('data-l10n-id') === 'item-menu-viewAttachment') { // zotero 7
+            sibling = mi
+          }
+
+          if (mi.style?.listStyleImage === 'url("chrome://zotero/skin/treeitem-attachment-pdf.png")') { // zotero 6
+            sibling = mi
+          }
         }
 
-        if (sibling) {
-          const alternate = createElement('menuitem')
+        if (!sibling) {
+          log('Error: sibling not found')
+          return
+        }
+
+        const copyattr = (elt: HTMLElement) => {
           for (const att of Array.from(sibling.attributes)) {
-            alternate.setAttribute(att.nodeName, att.nodeValue)
+            if (att.nodeName.match(/^data-l10n-/)) continue
+            elt.setAttribute(att.nodeName, att.nodeValue)
           }
-          alternate.setAttribute('zotero-open-pdf-external', 'true')
+        }
+        const alternate = createElement('menuitem')
+        copyattr(alternate)
+        alternate.setAttribute('data-open-pdf-alternate', 'true')
 
-          if (Zotero.Prefs.get('fileHandler.pdf')) { // existing Open option = external
-            log('adding external system opener')
-            alternate.setAttribute('label', Zotero.getString('locate.internalViewer.label') as string)
-            alternate.addEventListener('command', async event => { // eslint-disable-line @typescript-eslint/no-misused-promises
-              event.stopPropagation()
-              const items = ZoteroPane.getSelectedItems()
-              for (const item of items) {
-                const attachment = item.isAttachment() ? item : (await item.getBestAttachment())
-                if (attachment?.attachmentPath.match(/[.]pdf$/i)) {
-                  await Zotero.Reader.open(attachment.itemID, false, { openInWindow: false })
-                }
+        if (Zotero.Prefs.get('fileHandler.pdf')) { // existing Open option = external
+          log(`adding internal opener: ${Zotero.getString('locate.internalViewer.label')}`)
+          alternate.setAttribute('label', Zotero.getString('locate.internalViewer.label') as string)
+          alternate.addEventListener('command', async event => { // eslint-disable-line @typescript-eslint/no-misused-promises
+            event.stopPropagation()
+            const items = ZoteroPane.getSelectedItems()
+            for (const item of items) {
+              const attachment = item.isAttachment() ? item : (await item.getBestAttachment())
+              if (attachment?.attachmentPath.match(/[.]pdf$/i)) {
+                await Zotero.Reader.open(attachment.itemID, false, { openInWindow: false })
               }
-            }, false)
-          }
-          else { // existing Open option = internal
-            log('adding internal opener')
-            alternate.setAttribute('label', Zotero.getString('locate.externalViewer.label') as string)
-            alternate.addEventListener('command', async event => { // eslint-disable-line @typescript-eslint/no-misused-promises
-              event.stopPropagation()
-              const items = ZoteroPane.getSelectedItems()
-              for (const item of items) {
-                const attachment = item.isAttachment() ? item : (await item.getBestAttachment())
-                if (attachment?.attachmentPath.match(/[.]pdf$/i)) {
-                  Zotero.launchFile(attachment.getFilePath())
-                }
-              }
-            }, false)
-          }
-
-          sibling.parentNode.insertBefore(alternate, sibling.nextSibling)
-
-          for (const cmdline of (Zotero.Prefs.rootBranch.getChildList(Openers, {}, {}) as string[]).sort()) {
-            const opener = getOpener(cmdline)
-            if (!opener.label || !opener.cmdline) continue
-
-            log(`adding ${cmdline}: ${JSON.stringify(opener)}`)
-            const custom = createElement('menuitem')
-            for (const att of Array.from(sibling.attributes)) {
-              custom.setAttribute(att.nodeName, att.nodeValue)
             }
-            custom.setAttribute('label', opener.label)
-            custom.setAttribute('data-open-pdf', cmdline)
-            custom.addEventListener('command', async event => { // eslint-disable-line @typescript-eslint/no-misused-promises
-              event.stopPropagation()
+          }, false)
+        }
+        else { // existing Open option = internal
+          log(`adding external system opener: ${Zotero.getString('locate.externalViewer.label')}`)
+          alternate.setAttribute('label', Zotero.getString('locate.externalViewer.label') as string)
+          alternate.addEventListener('command', async event => { // eslint-disable-line @typescript-eslint/no-misused-promises
+            event.stopPropagation()
+            const items = ZoteroPane.getSelectedItems()
+            for (const item of items) {
+              const attachment = item.isAttachment() ? item : (await item.getBestAttachment())
+              if (attachment?.attachmentPath.match(/[.]pdf$/i)) {
+                Zotero.launchFile(attachment.getFilePath())
+              }
+            }
+          }, false)
+        }
 
-              try {
-                const target = event.target as HTMLSelectElement
-                const runtime = getOpener(target.getAttribute('data-open-pdf'))
-                if (!runtime.cmdline) throw new Error(`No opener for ${target.getAttribute('label')}`)
+        sibling.parentNode.insertBefore(alternate, sibling.nextSibling)
 
-                const args : string[] = unshell(runtime.cmdline)
-                log(`command: ${JSON.stringify(args)}`)
-                const cmd = args.shift()
-                const items = ZoteroPane.getSelectedItems()
-                for (const item of items) {
-                  const attachment = item.isAttachment() ? item : (await item.getBestAttachment())
-                  if (attachment?.attachmentPath.match(/[.]pdf$/i)) {
-                    exec(cmd, args.map((arg : string) => arg.toLowerCase() === '@pdf' ? attachment.getFilePath() as string : arg))
-                  }
+        for (const cmdline of (Zotero.Prefs.rootBranch.getChildList(Openers, {}, {}) as string[]).sort()) {
+          const opener = getOpener(cmdline)
+          if (!opener.label || !opener.cmdline) continue
+
+          log(`adding ${cmdline}: ${JSON.stringify(opener)}`)
+          const custom = createElement('menuitem')
+          copyattr(custom)
+          custom.setAttribute('label', opener.label)
+          custom.setAttribute('data-open-pdf', cmdline)
+          custom.addEventListener('command', async event => { // eslint-disable-line @typescript-eslint/no-misused-promises
+            event.stopPropagation()
+
+            try {
+              const target = event.target as HTMLSelectElement
+              const runtime = getOpener(target.getAttribute('data-open-pdf'))
+              if (!runtime.cmdline) throw new Error(`No opener for ${target.getAttribute('label')}`)
+
+              const args : string[] = unshell(runtime.cmdline)
+              log(`command: ${JSON.stringify(args)}`)
+              const cmd = args.shift()
+              const items = ZoteroPane.getSelectedItems()
+              for (const item of items) {
+                const attachment = item.isAttachment() ? item : (await item.getBestAttachment())
+                if (attachment?.attachmentPath.match(/[.]pdf$/i)) {
+                  exec(cmd, args.map((arg : string) => arg.toLowerCase() === '@pdf' ? attachment.getFilePath() as string : arg))
                 }
               }
-              catch (err) {
-                flash('Could not open attachments', (err.message as string) || 'unknown error')
-              }
-            }, false)
-            alternate.parentNode.insertBefore(custom, alternate.nextSibling)
-          }
+            }
+            catch (err) {
+              flash('Could not open attachments', (err.message as string) || 'unknown error')
+            }
+          }, false)
+          alternate.parentNode.insertBefore(custom, alternate.nextSibling)
         }
       }
       catch (err) {
