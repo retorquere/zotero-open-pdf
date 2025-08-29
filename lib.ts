1fb61ecca186d0a45a3b020e05f47e218ef76623
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2025-08-29 15:34:29 +0200

    debug log sender

--- lib.ts
+++ lib.ts
@@ -5,6 +5,9 @@
 import { MenuManager, MenuitemOptions } from 'zotero-plugin-toolkit'
 const Menu = new MenuManager()
 
+import { DebugLog } from 'zotero-plugin/debug-log'
+DebugLog.register('Open PDF', ['extensions.zotero.open-pdf.'])
+
 import unshell from 'shell-quote/parse'
 
 function log(msg) {
